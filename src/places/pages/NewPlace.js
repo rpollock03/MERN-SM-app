42807d0b4293d720736966e7d408588166bb645e
rpollock03 <49661708+rpollock03@users.noreply.github.com>
2020-05-10 11:46:39 +0100

    first commit

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function NewPlace() {
+
+
+    return <h2>New place works!</h2>
+}
+
+
+
+export default NewPlace
---
+++ src/user/pages/Users.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+function Users() {
+
+    return <h2>Users works!</h2>
+
+}
+
+export default Users

7aef995127157bef703ce375f574fc4392797d67
rpollock03 <49661708+rpollock03@users.noreply.github.com>
2020-05-17 12:32:18 +0100

    new hook form update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import Users from "./user/pages/Users"
 import NewPlace from "./places/pages/NewPlace"
 import UserPlaces from "./places/pages/UserPlaces"
+import UpdatePlace from "./places/pages/UpdatePlace"
 import MainNavigation from "./shared/components/Navigation/MainNavigation"
 
 function App() {
@@ -18,6 +19,9 @@ function App() {
         </Route>
         <Route path="/places/new" exact >
           <NewPlace />
+        </Route>
+        <Route path="/places/:placeId">
+          <UpdatePlace />
         </Route>
         <Redirect to="/" />
       </Switch>
